92bc14cb011ffbe2fa5f9a170f5de37e0e07dfd4
Melissa Key <melkey@i4.red.uits.iu.edu>
2018-11-05 18:04:40 -0500

    combined fnr-based methods

--- R/clean_classes.R
+++ R/clean_classes.R
@@ -98,8 +98,7 @@ clean_classes <- function(D, assignment, classes = 'all', tau = c(0.01, 0.05, 0.
       # 
       p_BH <- unlist(tapply(p, tau, p.adjust, method = "BH"))
       p_BY <- unlist(tapply(p, tau, p.adjust, method = 'BY'))
-    }) 
-    
+    })
     
     ###### original direction
     Zi_psi <- data.frame(
@@ -107,54 +106,37 @@ clean_classes <- function(D, assignment, classes = 'all', tau = c(0.01, 0.05, 0.
       instance = labels[assignment == k],
       index  = which(assignment == k)
     )
-    Zi_orig <- within(Zi_psi, {
+    Zi_psi <- within(Zi_psi[order(Zi_psi$Zi, decreasing = TRUE),], {
+      beta_BH <- beta0 * (Nk[k]:1) / Nk[k]
+      beta_BY <- beta_BH / cumsum(1 / 1:Nk[k]) 
+      Y_BH <- qbinom(beta_BH, Nk[k] - 1, psi_t['tau'], lower.tail = FALSE)
+      Y_BY <- qbinom(beta_BY, Nk[k] - 1, psi_t['tau'], lower.tail = FALSE)
+      keep_BH <- cumsum(Zi <= Y_BH) == 0
+      keep_BY <- cumsum(Zi <= Y_BY) == 0
       c.ia <- stats::qbinom(alpha, Nk[k]-1, 1 - psi_t['tau'])
-      # tau.hat <- Zi / (Nk[k] - 1)
-      # tau.tilde <- (psi_t['tau'] + tau.hat) / 2
-      # 
-      # z.ia <- stats::qbinom(alpha, Nk[k]-1, tau.tilde) - 1
-      # 
-      # c.ia <- pmin(z.0a, z.ia)
-      # 
       alpha.i <- stats::pbinom(c.ia, Nk[k] - 1, 1 - psi_t['tau'])
       p.i <- stats::pbinom(Zi, Nk[k] - 1, 1 - psi_t['tau'])
 
       tc <- psi_t['t']
       tau.bar <- 1 - psi_t['tau']
-      
-      alpha <- alpha
       alpha0 <- alpha0
-      # if(!is.null(colnames(D11))) label <- colnames(D11)
-      # else label <- paste0("N", k, ".i", 1:Nk[k])
       Nk <-  as.numeric(Nk[k])
       k <- factor(k)
+      
     })
-    Zi_beta <- within(Zi_psi[order(Zi_psi$Zi, decreasing = TRUE),], {
-      beta_BH <- beta0 * (Nk[k]:1) / Nk[k]
-      beta_BY <- beta_BH / cumsum(1 / 1:Nk[k]) 
-      Y <- qbinom(beta0, Nk[k] - 1, psi_t['tau'], lower.tail = FALSE)
-      Y_BH <- qbinom(beta_BH, Nk[k] - 1, psi_t['tau'], lower.tail = FALSE)
-      Y_BY <- qbinom(beta_BY, Nk[k] - 1, psi_t['tau'], lower.tail = FALSE)
-      Nk <- Nk[k]-1
-      keep_BH <- cumsum(Zi <= Y_BH) == 0
-      keep_BY <- cumsum(Zi <= Y_BY) == 0
-      tc <- psi_t['t']
-      tau.bar <- psi_t['tau']
-      k <- k
-    })
-    Zi_beta <- Zi_beta[order(Zi_beta$index),]
+    Zi_psi <- Zi_psi[order(Zi_psi$index),]
+
     list(
-      flipped = Zi,
-      alpha = Zi_orig,
-      beta = Zi_beta
+      fdr = Zi,
+      fnr = Zi_psi
     )
   })
   
-  result <- lapply(1:3, function(i) {
+  result <- lapply(1:2, function(i) {
     do.call("rbind", lapply(result, function(x) x[[i]]))
   })
-  names(result) <- c("melissa", "alpha", "beta")
+  names(result) <- c("fdr_method","fnr_methods")
 
-  result$melissa <- result$melissa[c("k", "index", "instance", 'tau', "Zi", "p", "p_BH", "p_BY", "Nk")]
+  result$fdr_method <- result$fdr_method[c("k", "index", "instance", 'tau', "Zi", "p", "p_BH", "p_BY", "Nk")]
   result
 }
