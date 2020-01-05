92ae254ba65c2fe3d135f4b02ce8d1edd51de6c9
Melissa Key <melkey@iu.edu>
2020-01-04 23:04:10 -0500

    added check for length of alpha0
    removed old commented code

--- R/clean_classes.R
+++ R/clean_classes.R
@@ -23,6 +23,10 @@ clean_classes <- function(D, assignment, classes = 'all', alpha0 = 0.05, beta0 =
   if(!(is.matrix(D) && isSymmetric(D) && is.numeric(D))) stop('D must be a symmetric matrix with numeric entries')
   if(length(assignment) != nrow(D)) stop('length(asssignment) != nrow(D)')
   if(min(D) < 0) stop("D should be a distance matrix with entries >= 0.")
+  if(length(alpha0) != 1) {
+    warning("Multiple values of alpha0 found.  Only the first is used.")
+    alpha0 <- alpha0[1]
+  }
   
   class_table <- table(assignment)
   
@@ -64,10 +68,7 @@ clean_classes <- function(D, assignment, classes = 'all', alpha0 = 0.05, beta0 =
 
 
     psi_t <- psi(D11[lower.tri(D11)], D21)
-    # m_tc <- c(quantile(D21, tau), psi_t['t'])
-    # names(m_tc)[length(m_tc)] <- paste0(psi_t['tau'] * 100, "%")
-    # pp <- sapply(1:Nk[k], function(i) colMeans(outer(D21[i,], m_tc, "<")))
-    
+
     Zi_psi <- data.frame(
       Zi = vapply(1:Nk[k], function(i) sum(D11[-i,i] < psi_t['t']), 0),
       instance = labels[assignment == k],
@@ -78,15 +79,11 @@ clean_classes <- function(D, assignment, classes = 'all', alpha0 = 0.05, beta0 =
       tau_hat <- Zi / Nk[k]
       tau_tilde <- (psi_t['tau'] + tau_hat) / 2
       a_tide <- stats::qbinom(alpha, Nk[k] - 1, tau_tilde)
-      # b <- stats::qbinom(alpha, Nk[k] - 1, 1 - psi_t['tau'], lower.tail = FALSE) 
       q <- 1 - stats::pbinom(Zi, Nk[k] - 1, 1 - psi_t['tau'])
-      # q_BH <- stats::p.adjust(q, method = 'BH')
       q_BY <- stats::p.adjust(q, method = 'BY')
-      # q_Bon <- stats::p.adjust(q, method = 'bonferroni')
 
       p <- stats::pbinom(Zi, Nk[k] - 1, psi_t['tau'])
       p_tilde <- stats::pbinom(Zi, Nk[k] - 1, tau_tilde)
-      # p_BH <- stats::p.adjust(p, method = 'BH')
       p_BY <- stats::p.adjust(p, method = 'BY')
       pt_BY <- stats::p.adjust(p, method = 'BY')
       p_Bon <- stats::p.adjust(p, method = 'bonferroni')
